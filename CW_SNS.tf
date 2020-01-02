cc25a16e64db7c91f92703c3debd5236aefbb126
Shubham Dixit <51513309+shubhdev79@users.noreply.github.com>
2020-01-02 19:11:56 +0530

    SNS _Topic Creation for Dashboard & Alarms

---
+++ CW_SNS.tf
@@ -0,0 +1,25 @@
+resource "aws_sns_topic" "sample_topic" {
+  name = "voice-vault_topic"
+  delivery_policy = <<EOF
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false,
+    "defaultThrottlePolicy": {
+      "maxReceivesPerSecond": 1
+    }
+  }
+}
+EOF
+      provisioner "local-exec" {
+      command = "aws sns subscribe --region eu-west-3 --topic-arn ${self.arn} --protocol email --notification-endpoint shubham.dixit@verint.com"
+}
+}
